6304f7b79df8ecff8a0d629f60ccb3b026c9fcd2
Purushottam Anand <purushottam.anand@D11-Purushottam.anand>
2022-04-14 19:32:21 +0530

    communication with master as IO

--- clusterApp.js
+++ clusterApp.js
@@ -4,6 +4,7 @@ import cluster from "cluster";
 import {benchmarkFunction} from './util.js';
 import OS from "os";
 const totalCPUs = OS.cpus().length;
+import PubSub from 'ipc-pubsub'
 
 if (cluster.isMaster) {
     console.log(`Number of CPUs is ${totalCPUs}`);
@@ -11,7 +12,21 @@ if (cluster.isMaster) {
 
     // Fork workers.
     for (let i = 0; i < totalCPUs - 2; i++) {
-        cluster.fork();
+        const workerId = i + 100
+        const worker = cluster.fork({workerId: workerId});
+        worker.on('message', async msg => {
+            console.log(`message called`)
+            if (msg.msgType === 'processIo') {
+                console.log(`console --- found`)
+                const ret = await makeApiRequest('abc')
+                msg.returned.value = ret
+                console.log(`reeeetuendeddd `, msg.returned.value)
+                worker.send({
+                    msgType: 'IO_PROCESSED',
+                    value: ret
+                })
+            }
+        })
     }
 
     cluster.on("exit", (worker, code, signal) => {
@@ -41,16 +56,26 @@ if (cluster.isMaster) {
     });
 
     app.get("/api/benchmark/:n", async function (req, res) {
-        const arrival = Date.now()
-        let n = parseInt(req.params.n);
-        while (n--) {
-            await benchmarkFunction()
-        }
-        const departure = Date.now()
-        return departure
+        const returned = {}
+        const ret = process.send({
+            msgType: 'processIo',
+            data: {arg: 5, workerId: process.env.workerId},
+            returned
+        })
+        await process.on('message', msg => {
+            if (msg.msgType === 'IO_PROCESSED') {
+                console.log(`asdfsa`, msg.value)
+                res.send(msg.ret)
+            }
+        })
+        res.send('Kuch nahi')
     });
 
     app.listen(port, () => {
         console.log(`App listening on port ${port}`);
     });
+}
+
+const makeApiRequest = async (arg) => {
+    return "makeApiResponse"
 }
