069ba4e7fad3076c3d664398c80b3df4450a7c13
Purushottam Anand <purushottam.anand@D11-Purushottam.anand>
2022-04-27 10:19:47 +0530

    batching with pm2

--- pm2app.js
+++ pm2app.js
@@ -6,7 +6,7 @@ import {makeIOCall} from "./util.js";
 import EventEmitter from 'events';
 import DataLoader from 'dataloader'
 
-const userLoader = new DataLoader(keys => myBatchGetUsers(keys))
+// const userLoader = new DataLoader(keys => myBatchGetUsers(keys))
 
 class MyEmitter extends EventEmitter {}
 
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/api/benchmark/:n", async function (req, res) {
-    const requestId = parseInt(req.params.n) + Date.now() + Math.random()%1000000
+    const requestId = req.params.n + Math.random()%1000000
     // console.log(`api called with nnn = `, req.params.n, " received at requestId - ", requestId, " ", new Date().toISOString())
     // const msg = {
     //     msgType: 'processIO',
@@ -33,69 +33,75 @@ app.get("/api/benchmark/:n", async function (req, res) {
     //         arg: { n: req.params.n }
     //     }
     // }
-    const result = await makeApiRequest(req.params.n)
-    res.json({output: result})
+    myEmitter.emit('PROCESS_REQUEST', {
+        requestId,
+        arg: req.params.n
+    })
+
+    myEmitter.on('IO_PROCESSED', function(msg) {
+        console.log(`while returning `, msg)
+        if(msg.requestId === requestId) {
+            res.json(msg.data)
+        }
+    })
+    // const result = await makeApiRequest(req.params.n)
+    // res.json({output: result})
 })
 
-// myEmitter.on('PROCESS_REQUEST', function(msg) {
-//     queue.push(msg)
-//     setInterval(
-//         async function () {
-//             let tempQueue = queue
-//             queue = []
-//             if (tempQueue.length > 0) {
-//                 await batchAndSendResponse(tempQueue);
-//                 tempQueue = []
-//             }
-//         },
-//         config.batchingInterval
-//     )
-// })
+myEmitter.on('PROCESS_REQUEST', function(msg) {
+    console.log(`PROCESS_REQUEST `, new Date())
+    queue.push(msg)
+    setInterval(
+        async function () {
+            let tempQueue = queue
+            queue = []
+            if (tempQueue.length > 0) {
+                await batchAndSendResponse(tempQueue);
+                tempQueue = []
+            }
+        },
+        config.batchingInterval
+    )
+})
 
-// const batchAndSendResponse = async (requestsQueue) => {
-//     console.log(`batchAndSendResponse called --- `, new Date().toISOString(), requestsQueue.length, requestsQueue)
-//     const batchedRequests = getBatchedRequests(requestsQueue)
-//     const promiseArray = []
-//     const batchedReqKeys = Object.keys(batchedRequests)
-//     for (let key of batchedReqKeys) {
-//         promiseArray.push(makeApiRequest(key)) // TODO: fix this to be arg from any object
-//     }
-//     const apiResponses = await Promise.all(promiseArray)
-//     let i = 0;
-//     for(let key of batchedReqKeys) {
-//         const apiResponse = apiResponses[i]
-//         i++;
-//         const relevantRequests = batchedRequests[key]
-//         for (let relevantRequest of relevantRequests) {
-//             const returnMessage = relevantRequest.request.msg
-//             const eventName = `REQUEST_PROCESSED_${returnMessage.requestId}`
-//             myEmitter.emit(eventName, {
-//                 msgType: 'IO_PROCESSED',
-//                 value: apiResponse,
-//                 requestId: returnMessage.requestId,
-//                 newMsgType: '',
-//                 workerId: returnMessage.data.workerId,
-//             })
-//         }
-//     }
-// }
+const batchAndSendResponse = async (requestsQueue) => {
+    console.log(`batchAndSendResponse called --- `, new Date().toISOString(), requestsQueue.length, requestsQueue)
+    const batchedRequests = getBatchedRequests(requestsQueue)
+    const promiseArray = []
+    const batchedReqKeys = Object.keys(batchedRequests)
+    for (let key of batchedReqKeys) {
+        promiseArray.push(makeApiRequest(key)) // TODO: fix this to be arg from any object
+    }
+    const apiResponses = await Promise.all(promiseArray)
+    let i = 0;
+    for(let key of batchedReqKeys) {
+        const apiResponse = apiResponses[i]
+        i++;
+        const relevantRequests = batchedRequests[key]
+        for (let relevantRequest of relevantRequests) {
+            myEmitter.emit('IO_PROCESSED', {
+                requestId: relevantRequest.request.requestId,
+                data: apiResponse
+            })
+        }
+    }
+}
 
-// const getBatchedRequests = (requestsQueue) => {
-//     const requestsMap = {}
-//     for (let request of requestsQueue) {
-//         if (requestsMap[request.msg.arg.n]) {
-//             requestsMap[request.msg.arg.n].push({arg: request.msg.arg.n, request})
-//         } else {
-//             requestsMap[request.msg.arg.n] = [{arg: request.msg.arg.n, request}]
-//         }
-//     }
-//     return requestsMap
-// }
+const getBatchedRequests = (requestsQueue) => {
+    const requestsMap = {}
+    for (let request of requestsQueue) {
+        if (requestsMap[request.arg]) {
+            requestsMap[request.arg].push({arg: request.arg, request})
+        } else {
+            requestsMap[request.arg] = [{arg: request.arg, request}]
+        }
+    }
+    return requestsMap
+}
 
 const makeApiRequest = async (arg) => {
     const output = await makeIOCall(arg)
-    // console.log("API output ", output)
     return output
 }
 
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
+app.listen(port, () => console.log(`Hello world app listening on porttt ${port}!`));
