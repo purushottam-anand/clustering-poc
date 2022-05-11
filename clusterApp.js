eecab621241806b4dbf6d910705967ba3d066845
Purushottam Anand <purushottam.anand@D11-Purushottam.anand>
2022-05-11 10:48:11 +0530

    All IO on master process

--- clusterApp.js
+++ clusterApp.js
@@ -3,91 +3,39 @@ import cluster from "cluster";
 import OS from "os";
 import {makeIOCall} from './util.js'
 import {config} from "./config.js";
-
+const clusterMap = {};
 if (cluster.isPrimary) {
     const totalCPUs = OS.cpus().length;
-    const clusterMap = {};
+    // console.log(`master pid ---`, process.pid)
     // Fork workers.
     const ioWorker = cluster.fork({workerId: 99, workerType: 'IO_WORKER'});
     
     //IO worker to master
-    ioWorker.on('message', async msg => {
-        // console.log(`master called for calling back cpu worker with response`, msg.msgType)
-        if (msg.msgType === 'IO_PROCESSED') {
-            //send msg back to CPU worker
-            // console.log(`master to cpu back --- `, msg)
-            const workerHere = clusterMap[msg.workerId]
-            workerHere.send({
-                msgType: msg.newMsgType,
-                msg
-            })
-        }
-    })
-
-    for (let i = 0; i < totalCPUs - 1; i++) {
-        const workerId = i + 100
+    // ioWorker.on('message', async msg => {
+    //     // // console.log(`master called for calling back cpu worker with response`, msg.msgType)
+    //     if (msg.msgType === 'IO_PROCESSED') {
+    //         //send msg back to CPU worker
+    //         // // console.log(`master to cpu back --- `, msg)
+    //         const workerHere = clusterMap[msg.workerId]
+    //         workerHere.send({
+    //             msgType: msg.newMsgType,
+    //             msg
+    //         })
+    //     }
+    // })
+    let queue = []
+    let currentWorkerId = 0
+    for (let i = 0; i < totalCPUs; i++) {
+        const workerId = i
         const worker = cluster.fork({workerId: workerId, workerType: 'CPU_WORKER'});
         clusterMap[workerId] = worker;
         // console.log(`worker created with ID`, workerId)
         //worker to master to callIO
         worker.on('message', async msg => {
             if (msg.msgType === 'callIO') {
-                //send to IO worker to process IO
-                const ioWorkerCalled = ioWorker.send({
-                    msgType: 'processIO',
-                    msg
-                })
-                // console.log(`event emitted for calling IO worker - `, ioWorkerCalled)
-            }
-        })
-    }
-
-    cluster.on("exit", (worker, code, signal) => {
-        // console.log(`worker ${worker.process.pid} died`);
-        // console.log("Let's fork another worker!");
-        cluster.fork();
-    });
-
-    cluster.on("error", (worker, code, signal) => {
-        // console.log(`worker ${worker.process.pid} died`);
-        // console.log("Let's fork another worker!");
-        // cluster.fork();
-    });
-} else {
-    const app = express();
-    app.get("/api/benchmark/:n", async function (req, res) {
-        // console.log(`api called with n = `, req.params.n, " received at workerId - ", process.env.workerId, new Date().toISOString())
-        const requestId = req.params.n + Math.random()%1000000
-        const arg = {n: req.params.n}
-
-        //worker to master
-        const ret = process.send({
-            msgType: 'callIO',
-            data: {arg: req.params.n, workerId: process.env.workerId},
-            requestId,
-            arg
-        })
-        // console.log(`Mater called for getting IO processed --- `, req.params.n)
-        await process.on('message', msg => {
-            const currentWorkerId = process.env.workerId
-            const relevantMessageType = `IO_PROCESSED_WORKER_ID_${currentWorkerId}`
-            if (msg.msg.newMsgType === relevantMessageType && msg.msg.requestId === requestId) {
-                res.json({output: msg.msg.value})
-            }
-        })
-    });
-    if (process.env.workerId != 99) {
-        app.listen(config.port, () => {
-            // console.log(`App listening on port ${config.port}`);
-        });
-    }
-
-    if (process.env.workerId == 99) {
-        let queue = []
-        process.on('message', async msg => {
-            if (msg.msgType === 'processIO') {
+                // console.log(`callIOOOO called --- `, msg)
                 queue.push(msg)
-                // console.log(`pushed to queue `, new Date().toISOString(), msg)
+                // // console.log(`pushed to queue `, new Date().toISOString(), msg)
                 setInterval(
                     async function () {
                         let tempQueue = queue
@@ -102,10 +50,101 @@ if (cluster.isPrimary) {
             }
         })
     }
+
+    const app = express();
+    app.listen(config.port, () => {
+        // // console.log(`App listening on port ${config.port}`);
+    });
+    app.get("/api/benchmark/:n", async function (req, res) {
+        // // console.log(`api called with n = `, req.params.n, " received at workerId - ", process.env.workerId, new Date().toISOString())
+        const requestId = req.params.n + Math.random()%1000000
+        const workerIdHere = currentWorkerId++
+        const randomisedWorker = clusterMap[workerIdHere % totalCPUs]
+        randomisedWorker.send({
+            msgType: 'processRequest',
+            n: req.params.n,
+            requestId
+        })
+        await randomisedWorker.on('message', msg => {
+            const currentWorkerId = process.env.workerId
+            const relevantMessageType = 'returnToMaster'
+            // console.log(`return to master --- `, msg)
+            if (msg.msgType === relevantMessageType && msg.requestId === requestId) {
+                // console.log(`returnToMaster -- `, msg)
+                res.json({output: msg.value})
+            }
+        })
+    });
+
+    cluster.on("exit", (worker, code, signal) => {
+        // // console.log(`worker ${worker.process.pid} died`);
+        // // console.log("Let's fork another worker!");
+        cluster.fork();
+    });
+
+    cluster.on("error", (worker, code, signal) => {
+        // // console.log(`worker ${worker.process.pid} died`);
+        // // console.log("Let's fork another worker!");
+        // cluster.fork();
+    });
+} else {
+    // if (process.env.workerId == 99) {
+    //     let queue = []
+    //     process.on('message', async msg => {
+    //         if (msg.msgType === 'processIO') {
+    //             queue.push(msg)
+    //             // // console.log(`pushed to queue `, new Date().toISOString(), msg)
+    //             setInterval(
+    //                 async function () {
+    //                     let tempQueue = queue
+    //                     queue = [] // TODO: check if we can miss requests in transient period !
+    //                     if (tempQueue.length > 0) {
+    //                         await batchAndSendResponse(tempQueue);
+    //                         tempQueue = []
+    //                     }
+    //                 },
+    //                 config.batchingInterval
+    //             )
+    //         }
+    //     })
+    // }
+
+    // master delegated request to a worker in round-robin
+    process.on('message', async msg => {
+        // console.log(`message in worker`, msg)
+        if (msg.msgType === 'processRequest') {
+            // console.log(`abcd`)
+            return processRequest(msg)
+        }
+        // // console.log(`Mater called for getting IO processed --- `, req.params.n)
+    })
 }
 
+const processRequest = async (msg) => {
+    // console.log(`processRequest message in worker`, msg)
+    const requestId = msg.requestId
+    const arg = {n: msg.n}
+    //worker to master for any IO call
+    const ret = process.send({
+        msgType: 'callIO',
+        data: {arg: msg.n, workerId: process.env.workerId},
+        requestId
+    })
+    process.on('message', async msg => {
+        if (msg.msgType === `IO_PROCESSED_WORKER_ID_${process.env.workerId}` && msg.requestId === requestId) {
+            // console.log(`io processed message to master to return to client`)
+            process.send({
+                msgType: 'returnToMaster',
+                requestId,
+                value: msg.value
+            })
+            // console.log(`io sent`)
+        }
+    })
+}
 const batchAndSendResponse = async (requestsQueue) => {
-    // console.log(`batchAndSendResponse called --- `, new Date().toISOString(), requestsQueue.length, requestsQueue)
+    // // console.log(`batchAndSendResponse called --- `, new Date().toISOString(), requestsQueue.length, requestsQueue)
+    // console.log(`request queue -- `, requestsQueue)
     const batchedRequests = getBatchedRequests(requestsQueue)
     const promiseArray = []
     const batchedReqKeys = Object.keys(batchedRequests)
@@ -119,31 +158,36 @@ const batchAndSendResponse = async (requestsQueue) => {
         i++;
         const relevantRequests = batchedRequests[key]
         for (let relevantRequest of relevantRequests) {
-            const returnMessage = relevantRequest.request.msg
-            const msgType = `IO_PROCESSED_WORKER_ID_${returnMessage.data.workerId}`
-            process.send({
-                msgType: 'IO_PROCESSED',
+            // console.log(`relevant request -- `, relevantRequest)
+            const returnMessage = apiResponse
+            // console.log(`api response --- `, apiResponse)
+            const msgType = `IO_PROCESSED_WORKER_ID_${relevantRequest.request.data.workerId}`
+            const workerHere = clusterMap[relevantRequest.request.data.workerId]
+            workerHere.send({
+                msgType: msgType,
                 value: apiResponse,
-                requestId: returnMessage.requestId,
+                requestId: relevantRequest.request.requestId,
                 newMsgType: msgType,
-                workerId: returnMessage.data.workerId,
+                workerId: relevantRequest.request.data.workerId,
             })
         }
     }
 }
 
 const getBatchedRequests = (requestsQueue) => {
+    // console.log(`rewuestsQueue --- `, requestsQueue)
     const requestsMap = {}
     for (let request of requestsQueue) {
-        if (requestsMap[request.msg.arg.n]) {
-            requestsMap[request.msg.arg.n].push({arg: request.msg.arg.n, request})
+        if (requestsMap[request.data.arg]) {
+            requestsMap[request.data.arg].push({arg: request.data.arg, request})
         } else {
-            requestsMap[request.msg.arg.n] = [{arg: request.msg.arg.n, request}]
+            requestsMap[request.data.arg] = [{arg: request.data.arg, request}]
         }
     }
     return requestsMap
 }
 const makeApiRequest = async (arg) => {
+    // console.log(`makeAPi tequest =-- `, arg)
     const output = await makeIOCall(arg)
     // console.log("API output ", output)
     return output
